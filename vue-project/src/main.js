db6098354339466eec330e757ec9d1fc10692d27
shinhw91 <shinhw91@nave.com>
2024-02-02 01:43:26 +0900

    240201 뷰 수업(1) 복습중

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,3 +1,11 @@
+// npm install : package.json 패키지 정보 설치(node_modules)
+// npm run serve : 프로젝트 실행
+
+// 실행순서
+// 1. main.js
+// 2. import 파일(App.vue, router -> index.js)
+// 3. import 파일(views)
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
