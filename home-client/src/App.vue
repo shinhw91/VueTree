a3ba9a6b19fb7f4ada0dc0e1dbd5140c211cac5d
shinhw91 <shinhw91@nave.com>
2024-02-05 21:42:50 +0900

    240205 노드-뷰 복습 진행중

---
+++ home-client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from './layouts/HeaderComponent.vue'
+
+export default {
+  components: {
+    Header
+  }
+}
+</script>
---
+++ home-client/src/views/UserListView.vue
@@ -0,0 +1,30 @@
+<!-- UserListView.vue -->
+<template>
+    <div class="container">
+        <h1>전체 회원 조회</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            userList : []
+        }
+    },
+    created() {
+        this.getUserList(); // 비동기작업
+    },
+    methods: {
+        async getUserList() {
+            // 동기작업(내부)
+            let result = await axios.get('http://localhost:5000/users')
+                                  .catch(err => console.log(err));
+            let list = result.data;
+            this.userList = list;
+        }
+    }
+}
+</script>
